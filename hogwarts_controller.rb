5afd23423b24265d78ad466f70c6ca45901325f2
Luis Tejero <luisfaridta@gmail.com>
2018-05-03 11:36:26 +0100

    student registration page created

--- hogwarts_controller.rb
+++ hogwarts_controller.rb
@@ -8,3 +8,14 @@ get '/students' do
   @students = Student.all
   erb(:index)
 end
+
+# CREATE a new student
+get "/students/new" do
+  erb(:new)
+end
+
+post "/students" do
+  @student = Student.new(params)
+  @student.save()
+  erb (:create)
+end

f3b72ae39de5639e3c5f8d09a2b3ec884bd2ef07
Luis Tejero <luisfaridta@gmail.com>
2018-05-03 11:55:23 +0100

    houses table and class created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require("pry")
 require_relative("../models/student")
+require_relative("../models/house")
 
 
 student1 = Student.new({
@@ -16,7 +17,33 @@ student1 = Student.new({
     "age" => 24
     })
 
+
+house1 = House.new({
+  "name" => "Gryffindor",
+  "url" => "https://i.ebayimg.com/images/g/L50AAOSwRsZZ3NTM/s-l1600.jpg"
+  })
+
+house2 = House.new({
+  "name" => "RavenClaw",
+  "url" => "https://i.pinimg.com/originals/e7/52/93/e7529322bb6c96e6f299a0d8b0e83f59.jpg"
+  })
+
+house3 = House.new({
+  "name" => "Slytherin",
+  "url" => "https://chrisy58.files.wordpress.com/2016/02/harry-potter-house-logos-slytherin-3.png"
+  })
+
+house4 = House.new({
+  "name" => "Hufflepuff",
+  "url" => "https://images-na.ssl-images-amazon.com/images/I/A1ZD6TqijzL._SL1500_.jpg"
+  })
+
+  # house1.save()
+  # house2.save()
+  # house3.save()
+  # house4.save()
+  # student1.save
+  # student2.save
+
 binding.pry
 nil
-    # student1.save
-    # student2.save
